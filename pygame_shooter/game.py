c58a689792155001c702b1a14b53f5d77533750b
Robert Bunch <rdbunch@gmail.com>
2018-11-21 10:22:52 -0500

    init repo

---
+++ pygame_shooter/game.py
@@ -0,0 +1,52 @@
+# 1. Include pygame
+# we needed pip to get this for us because Python doesnt ship with it
+import pygame
+
+# 2. Initialize Pygame.
+# Why do we need to do this? Because they told us to.
+pygame.init()
+
+# 3. Make a screen with a size. The size MUST be a tuple
+screen_size = (512,480)
+pygame_screen = pygame.display.set_mode(screen_size)
+# set the title of the window that opens...
+pygame.display.set_caption('Robin Hood')
+
+# ========VARIABLES FOR OUR GAME==========
+background_image = pygame.image.load('background.png')
+hero_image = pygame.image.load('hero.png')
+goblin_image = pygame.image.load('goblin.png')
+monster_image = pygame.image.load('monster.png')
+arrow_image = pygame.image.load('arrow.png')
+heroLoc = {
+    'x': 0,
+    'y': 0
+}
+
+# =========MAIN GAME LOOP==========
+game_on = True
+# the loop will run as long as our bool is true
+while game_on:
+    # we are in the game loop from here on out!
+    # 5. Listen for events and quit if the user clicks the x
+    # =======EVENT CHECKER=========
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                # THE USER CLICKED THE RED DOT!
+                # These aren't the droids were looking for. quit.
+                game_on = False
+        elif event.type == pygame.KEYDOWN:
+            # the user pressed a key!!!
+            print event.key
+            if event.key == 275:
+                # the user pressed the right arrow!!! Move our dude right
+                heroLoc['x'] += 10
+    # ==========DRAW STUFF===========
+    # we use blit to draw on the screen. blit = block image transfer
+    # blit is a method, that takes 2 arg:
+    # 1. What to draw
+    # 2. Where to draw it
+    # in the docs... SURFACE = our "pygame_screen"
+    pygame_screen.blit(background_image,[0,0])
+    pygame_screen.blit(hero_image,[heroLoc['x'],heroLoc['y']])
+    pygame.display.flip()
